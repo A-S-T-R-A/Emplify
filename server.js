3263326ad32e102e97f557a8cb82f49d689fe838
jesuitdelray <jesuitdelray@gmail.com>
2024-03-23 18:47:54 +0200

    inter

--- server.js
+++ server.js
@@ -2,47 +2,40 @@ const express = require("express")
 const puppeteer = require("puppeteer")
 const app = express()
 const PORT = process.env.PORT || 3000
+console.log("server is working")
 
+app.use(express.static("public"))
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Server is running. Use POST /generate-pdf to generate a PDF.")
-})
-
 app.post("/generate-pdf", async (req, res) => {
-    console.log("Received request to generate PDF")
+    const content = req.body.content
 
     try {
-        console.log("Launching browser...")
         const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            executablePath: puppeteer.executablePath(),
+            args: [
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--single-process",
+            ],
         })
 
-        console.log("Browser launched.")
         const page = await browser.newPage()
-        console.log("New page opened.")
 
-        console.log("Setting content...")
-        await page.setContent("<h1>Hello World</h1>")
-        console.log("Content set.")
+        await page.setContent(content, {
+            waitUntil: "networkidle0",
+        })
 
-        console.log("Generating PDF...")
         const pdf = await page.pdf({ format: "A4", printBackground: true })
-        console.log("PDF generated.")
 
         await browser.close()
-        console.log("Browser closed.")
 
         res.set({
             "Content-Type": "application/pdf",
-            "Content-Disposition": 'attachment; filename="generated-document.pdf"',
+            "Content-Disposition": 'attachment; filename="my-document.pdf"',
         })
-
-        console.log("Sending PDF to client...")
         res.send(pdf)
     } catch (error) {
-        console.error("PDF Generation Error:", error)
         res.status(500).send("An error occurred during the PDF generation.")
     }
 })
