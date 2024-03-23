f2a5403de5d4588b3ce1aebf31ba68d136009670
jesuitdelray <jesuitdelray@gmail.com>
2024-03-23 16:02:53 +0200

    add console.logs

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.use(express.static("public"))
 app.use(bodyParser.json())
 
 app.post("/generate-pdf", async (req, res) => {
+    console.log("Received request to generate PDF")
     const content = req.body.content
 
     try {
+        console.log("Launching browser...")
         const browser = await puppeteer.launch({
             args: [
                 "--no-sandbox",
@@ -19,19 +21,29 @@ app.post("/generate-pdf", async (req, res) => {
                 "--single-process",
             ],
         })
-        const page = await browser.newPage()
+        console.log("Browser launched.")
 
+        const page = await browser.newPage()
+        console.log("New page opened.")
+
+        console.log("Setting content...")
         await page.setContent(content, {
             waitUntil: "networkidle0",
         })
+        console.log("Content set.")
 
+        console.log("Generating PDF...")
         const pdf = await page.pdf({ format: "A4", printBackground: true })
+        console.log("PDF generated.")
+
         await browser.close()
+        console.log("Browser closed.")
 
         res.set({
             "Content-Type": "application/pdf",
             "Content-Disposition": 'attachment; filename="generated-document.pdf"',
         })
+        console.log("Sending PDF to client...")
         res.send(pdf)
     } catch (error) {
         console.error("PDF Generation Error:", error)
