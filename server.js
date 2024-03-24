ddab62c638b09a68d4a66a331d3398eedb8b8ba5
jesuitdelray <jesuitdelray@gmail.com>
2024-03-24 10:30:33 +0200

    inter

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ app.post("/generate-pdf", async (req, res) => {
 
     try {
         const browser = await puppeteer.launch({
-            args: [
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-                "--single-process",
-            ],
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
         })
 
         const page = await browser.newPage()
