aa9dbf035b3ed8e209bd0b2778a8a54ac29b53e6
jesuitdelray <jesuitdelray@gmail.com>
2024-03-24 11:02:18 +0200

    inter

--- server.js
+++ server.js
@@ -7,34 +7,17 @@ console.log("server is working")
 app.use(express.static("public"))
 app.use(express.json())
 
+const fs = require("fs")
+
 app.post("/generate-pdf", async (req, res) => {
-    const content = req.body.content
-    console.log("Received content for PDF generation:", content)
+    const staticPDF = fs.readFileSync("static-pdf-file.pdf")
 
-    try {
-        const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        })
+    res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": 'attachment; filename="my-document.pdf"',
+    })
 
-        const page = await browser.newPage()
-
-        await page.setContent(content, {
-            waitUntil: "networkidle0",
-        })
-
-        const pdf = await page.pdf({ format: "A4", printBackground: true })
-
-        await browser.close()
-
-        res.set({
-            "Content-Type": "application/pdf",
-            "Content-Disposition": 'attachment; filename="my-document.pdf"',
-        })
-        res.send(pdf)
-    } catch (error) {
-        console.error("Error during PDF generation:", error)
-        res.status(500).send("An error occurred during the PDF generation.")
-    }
+    res.send(staticPDF)
 })
 
 app.use((err, req, res, next) => {
