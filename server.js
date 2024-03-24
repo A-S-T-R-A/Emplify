e5562b9de6b8dc102ca8090ae3083d0dd29fae89
jesuitdelray <jesuitdelray@gmail.com>
2024-03-24 11:00:01 +0200

    inter

--- server.js
+++ server.js
@@ -37,6 +37,11 @@ app.post("/generate-pdf", async (req, res) => {
     }
 })
 
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("Something broke!")
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
 })
