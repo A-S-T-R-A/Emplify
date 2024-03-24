9735383d8cb10c99e2a1fd3979932723ee5624f4
jesuitdelray <jesuitdelray@gmail.com>
2024-03-24 13:21:18 +0200

    inter

--- server.js
+++ server.js
@@ -1,45 +1,36 @@
 const express = require("express")
 const puppeteer = require("puppeteer")
 const app = express()
-const PORT = process.env.PORT || 8080
-console.log("server is working")
+const port = 3000
 
-app.use(express.static("public"))
 app.use(express.json())
+app.use(express.static("public"))
 
 app.post("/generate-pdf", async (req, res) => {
-    const content = req.body.content
-    console.log("Received content for PDF generation:", content)
+    const { content } = req.body
+    if (!content) {
+        return res.status(400).send("Bad Request: No content provided")
+    }
 
     try {
-        const browser = await puppeteer.launch({ headless: false })
-
+        const browser = await puppeteer.launch()
         const page = await browser.newPage()
-
         await page.setContent(content, {
             waitUntil: "networkidle0",
         })
-
-        const pdf = await page.pdf({ format: "A4", printBackground: true })
+        const pdfBuffer = await page.pdf({ format: "A4" })
 
         await browser.close()
 
-        res.set({
-            "Content-Type": "application/pdf",
-            "Content-Disposition": 'attachment; filename="my-document.pdf"',
-        })
-        res.send(pdf)
+        res.setHeader("Content-Type", "application/pdf")
+        res.setHeader("Content-Disposition", "attachment; filename=document.pdf")
+        res.send(pdfBuffer)
     } catch (error) {
-        console.error("Error during PDF generation:", error)
-        res.status(500).send("An error occurred during the PDF generation.")
+        console.error("Error generating PDF:", error)
+        res.status(500).send("Server Error: Could not generate PDF")
     }
 })
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send("Something broke!")
-})
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
 })
