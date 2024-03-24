a0007bc5db1912f34b0490cd5a4b77cdf7335e26
jesuitdelray <jesuitdelray@gmail.com>
2024-03-24 13:29:18 +0200

    inter

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
-app.use(express.json())
+app.use(bodyParser.json())
 
-app.post("/generate-pdf", (req, res) => {
-    try {
-        console.log("Received content for PDF generation:", req.body.content)
-        res.status(200).send("PDF generation request received successfully.")
-    } catch (error) {
-        console.error("Error during PDF generation:", error)
-        res.status(500).send("An error occurred during the PDF generation.")
-    }
+app.post("/send-data", (req, res) => {
+    const data = req.body
+    console.log("Received data from client:", data)
+    res.send("Data received successfully!")
 })
 
+// Обработчик ошибок
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("Something broke!")
+})
+
+// Запуск сервера
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
 })
